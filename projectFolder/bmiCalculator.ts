149f2d2e57117603393567994cf0077dc11cb35a
Ville Laitinen <ville.o.laitinen@edu.turkuamk.fi>
2023-01-22 20:04:42 +0200

    9.1 complete

---
+++ projectFolder/bmiCalculator.ts
@@ -0,0 +1,15 @@
+const calculateBmi = (heightInCm: number, weightInKg: number): string => {
+  const bmi: number = weightInKg / (heightInCm / 100) ** 2;
+
+  if (bmi >= 30) {
+    return 'Obese';
+  } else if (bmi >= 25) {
+    return 'Overweight (Pre-obese)';
+  } else if (bmi >= 18.5) {
+    return 'Normal (healthy weight)';
+  } else {
+    return 'Underweight';
+  }
+};
+
+console.log(calculateBmi(180, 74));
